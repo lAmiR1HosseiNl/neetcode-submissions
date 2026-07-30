998e6e552a0da131f3fa2e878ab1ce9557f5165e
Amir Hossein Raeghi <94464208+lAmiR1HosseiNl@users.noreply.github.com>
2026-07-30 20:55:57 +0330

    Add: number-of-senior-citizens - submission-1

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    @staticmethod
+    def countSeniors(details: list[str]) -> int:
+        count_greater_than_60 = 0
+        for item in range(len(details)):
+            if int(details[item][11:13]) > 60:
+                count_greater_than_60 += 1      
+        return (count_greater_than_60)
