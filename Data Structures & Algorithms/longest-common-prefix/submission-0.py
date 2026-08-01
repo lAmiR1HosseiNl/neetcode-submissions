bc0179e72da8baba0bc37fe56f52703967bda7e5
Amir Hossein Raeghi <94464208+lAmiR1HosseiNl@users.noreply.github.com>
2026-08-01 14:11:27 +0330

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    @staticmethod
+    def longestCommonPrefix(strs: list[str]) -> str:
+        min_len_item = min(strs)
+        min_prefix = ''
+        for word in strs:
+            new_word = ''
+            for char in range(len(min_len_item)):
+                if word == strs[0]:
+                    if word[char] == min_len_item[char]:
+                        new_word += (min_len_item[char])
+                    else:
+                        min_prefix = new_word          
+                        break         
+                else:
+                    if word[char] == min_len_item[char]:
+                        new_word += (min_len_item[char])
+                    else:
+                        min_prefix = min(min_prefix, new_word)
+                        break
+            if word == strs[0]:
+                min_prefix = new_word
+            else:
+                min_prefix = min(min_prefix, new_word)
+
+        return (min_prefix)            
